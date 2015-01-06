281bdc37085eaf2b8d2e8c8e0cfa9155098b52ea
Malachi Gray <graymd.cfi@gmail.com>
2015-01-06 12:05:30 -0500

    first commit

---
+++ class2.rb
@@ -0,0 +1,15 @@
+# [] - brackets
+# {} - curly brackets
+array1 = [1,2,3,"jesse",4, "string"]
+puts array1.inspect
+puts array1[0]
+puts array1[4].inspect
+puts array1[3].inspect
+
+students = ["jesse", "adam", "jackie", "troy", "matt", "ryan"]
+puts students[2]
+
+students.push("robbi")
+puts students.inspect
+students.push("stephanie", "william")
+puts students.inspect
